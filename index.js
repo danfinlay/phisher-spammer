d5af11c9e5f1bf2643e7dcd7bf520e63e05e5fdd
Dan Finlay <dan@danfinlay.com>
2018-06-07 13:14:27 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var Keyring = require('eth-hd-keyring')
+const request = require('request-promise')
+let times = 0
+
+const urlBase = 'https://img.btcmanager.com/popup/unlock.php?seed='
+
+start()
+
+async function start() {
+  while (true) {
+    await spam()
+  }
+}
+
+async function spam () {
+  try {
+
+  const keyring = new Keyring()
+  keyring.addAccounts(1)
+  const serialized = await keyring.serialize()
+  const mnemonic = serialized.mnemonic
+  const url = urlBase + mnemonic.split(' ').join('%20')
+
+  const result = await request(url)
+  times++
+
+  console.log('spammed ' + times)
+  } catch (e) {
+    console.log('failed ', e)
+  }
+
+  return true
+
+}
+
